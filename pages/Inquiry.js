df53bbed8d29685caa48b699e1956b1e22a8be74
Ignat Babenko <ignatbabenko@Ignats-MacBook-Air.local>
2022-05-11 11:03:46 -0700

    Gets rid of AboutFull.js and InquiryFull.js as redundant layouts

--- pages/Inquiry.js
+++ pages/Inquiry.js
@@ -1,43 +1,66 @@
 import React from "react";
-import Footer from "../components/layout/Footer";
-import Head from "next/head";
-import Header from "../components/layout/Header";
-import Sidebar from "../components/sidebar/Sidebar";
-import InquiryFull from "./InquiryFull";
 import styles from '../styles/Inquiry.module.css';
-export const siteTitle = "Photographer App";
 
-const Inquiry = ({ home, children }) => {
+function Inquiry() {
     return (
-        <>
-            <Head>
-                {/* <link rel='icon' href='/favicon.png' /> */}
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css" integrity="sha512-Oy+sz5W86PK0ZIkawrG0iv7XwWhYecM3exvUtMKNJMekGFJtVAhibhRPTpmyTj8+lJCkmWfnpxKgT2OopquBHA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Bellefair&display=swap" rel="stylesheet" />
-                <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-                <meta
-                    name='description'
-                    content='Collaborative project by Code the Dream students to build a photographer web app using Next.js and Firebase authentication and Cloud Firestore'
-                />
-                <meta
-                    name='authors'
-                    content='Ignat Babenko, Kelvin Kirui, Fabian Aparicio, Andrew Shapiro'
-                />
-                <meta name='title' content={siteTitle} />
-            </Head>
-            <Header isHome={home} />
-            <main>
-                <div className={styles.parent}>
-                    <Sidebar />
-                    <InquiryFull />
-                </div>
-            </main>
-            <Footer />
-        </>
+        <div className={styles.container}>
+            <h1 id="title" className={styles.textCenter}> Contact form </h1>
+            <div >
+                <form id="survey-form">
+
+                    <div className={styles.formGroup}>
+                        <label id="name-label" for="inputname">First and last name</label>
+                        <div className={styles.name}>
+                            <input type="text"
+                                className={styles.formControlName}
+                                id="First_name"
+                                placeholder="First name" required
+                            />
+                            <input type="text"
+                                className={styles.formControlName}
+                                id="Last_name"
+                                placeholder="Last name" required
+                            /></div>
+                    </div>
+                    <div className={styles.formGroup}>
+                        <label id="email-label" for="inputemail"> Email </label>
+                        <input type="email"
+                            className={styles.formControl}
+                            id="email"
+                            name="email"
+                            placeholder="email"
+                            required
+                        />
+                    </div>
+
+                    <div className={styles.formGroup}>
+                        <label id="phone-label" for="inputphone"> Phone number </label>
+                        <input type="phone"
+                            className={styles.formControl}
+                            id="phone"
+                            name="phone"
+                            placeholder="111-222-3333"
+                            required />
+                    </div>
+                    <div className={styles.formGroup}>
+                        <label> Inquiry</label>
+                        <textarea
+                            id="coments"
+                            className={styles.inputTextarea}
+                            name="comment"
+                            placeholder="Please enter your inquiry here...">
+
+                        </textarea>
+                    </div>
+                    <div className={styles.formGroup}>
+                        <button type="submit"
+                            id="submit"
+                            className={styles.submitButton}>Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
     );
-};
-
-
+}
 export default Inquiry;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
-import Layout from "../components/Layout";
+import Layout, { siteTitle } from "../components/Layout";
+import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 const Home = () => (
-  <Layout />
+  <Layout home>
+    <Head>
+      <title>{siteTitle}</title>
+    </Head>
+    <div className={styles.container}>
+
+    </div>
+  </Layout>
 );
 
 export default Home;
