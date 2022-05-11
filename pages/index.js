19aa040c4c0a37ab0956ef7f7ed43ca4e49b3917
Ignat Babenko <ignatbabenko@Ignats-MacBook-Air.local>
2022-05-11 11:20:01 -0700

    Gets rid of Layout wrapper in index.js and upper case letters in the pages names

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 const Home = () => (
-  <Layout home>
+  <>
     <Head>
       <title>{siteTitle}</title>
     </Head>
     <div className={styles.container}>
 
     </div>
-  </Layout>
+  </>
 );
 
 export default Home;
