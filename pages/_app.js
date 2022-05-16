03721cb10c651fad1ac1304727ba30735684f642
kirin608 <andrewshapi@gmail.com>
2022-05-16 17:46:58 -0400

    aded toastify

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 import { SessionProvider } from "next-auth/react"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Layout>
         <Component {...pageProps} />
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </Layout>
     </SessionProvider>
   )
